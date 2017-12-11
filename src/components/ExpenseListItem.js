7827d9507e1ab600f844114dec02e39b559f1171
aspdevlpr <asp.netdeveloper@outlook.com>
2017-12-11 16:53:03 +0530

    format 4

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -8,7 +8,7 @@ const ExpenseListItem = ({id, description, amount, createdAt}) => {
             <h3><Link to={`/edit/${id}`}>{description}</Link></h3>
             <p>
                 {numeral(amount / 100).format("$0,0.00")} - 
-                {moment(createdAt).format("MMMM Do, YYYY")}
+                {moment(createdAt).format("MMM Do, YYYY")}
             </p>           
             
         </div>
